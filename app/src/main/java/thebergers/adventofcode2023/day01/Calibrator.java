0bbbb60292bd7ccebfbf8673fbb880fd057c44c8
Charles Berger <c.berger@crossflight.com>
2023-12-02 14:51:54 +0000

    Day One Part 2

--- app/src/main/java/thebergers/adventofcode2023/day01/Calibrator.java
+++ app/src/main/java/thebergers/adventofcode2023/day01/Calibrator.java
@@ -1,32 +1,67 @@
 package thebergers.adventofcode2023.day01;
 
+import com.google.common.annotations.VisibleForTesting;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
+@Slf4j
 public class Calibrator {
 
     @Setter
     private List<String> data = new LinkedList<>();
 
-    Calibrator() {
+    @Getter(value = AccessLevel.PACKAGE, onMethod_ = @VisibleForTesting)
+    private String conversion;
 
+    private final Map<String, String> wordConversions = new HashMap<>();
+
+    Calibrator() {
+        loadWordConversions();
     }
 
     Calibrator(List<String> data) {
+        this();
         this.data = data;
     }
+
+    private void loadWordConversions() {
+        wordConversions.put("one", "o1e");
+        wordConversions.put("two", "t2o");
+        wordConversions.put("three", "t3e");
+        wordConversions.put("four", "4");
+        wordConversions.put("five", "5e");
+        wordConversions.put("six", "6");
+        wordConversions.put("seven", "s7n");
+        wordConversions.put("eight", "e8t");
+        wordConversions.put("nine", "n9e");
+    }
+
     Integer calibrateLine(String line) {
 
-        String noChars = line.replaceAll("[A-Za-z]", "");
-        return Integer.parseInt("%s%s".formatted(noChars.substring(0, 1), noChars.substring(noChars.length() - 1)));
+        String wordsToDigits = convertWordsToDigits(line);
+        conversion = wordsToDigits.replaceAll("[A-Za-z]", "");
+        String answer = "%s%s".formatted(conversion.substring(0, 1), conversion.substring(conversion.length() - 1));
+        LOG.debug("input={}, converted={} result={}, answer={}", line, wordsToDigits, conversion, answer);
+        return Integer.parseInt(answer);
+    }
+
+    private String convertWordsToDigits(String line) {
+        String output = line;
+        for (Map.Entry<String, String> conversion : wordConversions.entrySet()) {
+            output = output.replaceAll(conversion.getKey(), conversion.getValue());
+        }
+        LOG.debug("input={},output={}", line, output);
+        return output;
     }
 
     public int calibrate() {
-        //return data.stream().map(this::calibrateLine).mapToInt(Integer::parseInt).sum();
         return data.stream().mapToInt(this::calibrateLine).sum();
     }
 }
--- app/src/test/java/thebergers/adventofcode2023/day01/CalibratorTest.java
+++ app/src/test/java/thebergers/adventofcode2023/day01/CalibratorTest.java
@@ -65,6 +65,60 @@ public class CalibratorTest {
 
             assertEquals(142, result);
         }
+    }
+
+    @Nested
+    class Part2 {
+        @BeforeAll
+        public static void setupAll() {
+
+            inputData.put("two1nine,219", 29);
+            inputData.put("eightwothree,823", 83);
+            inputData.put("abcone2threexyz,123", 13);
+            inputData.put("xtwone3four,2134", 24);
+            inputData.put("4nineeightseven2,49872", 42);
+            inputData.put("zoneight234,18234", 14);
+            inputData.put("7pqrstsixteen,76", 76);
+            inputData.put("oneight,18", 18);
+            inputData.put("twone,21", 21);
+            inputData.put("threeight,38", 38);
+            inputData.put("fiveight,58", 58);
+            inputData.put("sevenine,79", 79);
+            inputData.put("eightwo,82", 82);
+            inputData.put("eightwone,821", 81);
+            inputData.put("eighthree,83", 83);
+            inputData.put("nineight,98", 98);
+            inputData.put("sevenineight,798", 78);
+            inputData.put("twoneighthreeight,21838", 28);
+        }
+        @AfterAll
+        public static void tearDown() {
+
+            inputData.clear();
+        }
+
+        @ParameterizedTest
+        @MethodSource("thebergers.adventofcode2023.day01.CalibratorTest#calibrateLineArguments")
+        public void calibrateLineShouldCalculateCalibrationValueForLine(String input, Integer expectedResult) {
+
+            String[] data = input.split(",");
+            String line = data[0];
+            String expectedConversion = data[1];
+            Integer actualResult = underTest.calibrateLine(line);
+
+            assertEquals(expectedConversion, underTest.getConversion());
+            assertEquals(expectedResult, actualResult);
+        }
+        @Test
+        public void calibrateShouldCalculateTotalForAllInputLines() {
+
+            List<String> input = new ArrayList<>(inputData.keySet());
+            underTest.setData(input);
+
+            int result = underTest.calibrate();
+
+            assertEquals(945, result);
+        }
 
     }
 }
